c5a9e2ffb09b698ca0990d4d99a4088e8b65bfd5
Vijay <vijayvee@gmail.com>
2018-08-03 17:11:23 +0530

    Testing confusion matrix code

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -1,4 +1,9 @@
-import matplotlib.pyplot as plt
+#!/usr/bin/python
+import glob
+import os
+import sys
+import numpy as np
+from behavior_recognition.tools.utils import plot_confusion_matrix
 import pickle
 
 def load_preds_labels(pickle_name):
@@ -12,4 +17,18 @@ def collect_preds_labels(pickle_path):
         preds_labels.extend(load_preds_labels(pkl))
     return preds_labels
 
+def confusion_matrix_master(pickle_path):
+    preds_labels = collect_preds_labels(pickle_path)
+    preds = [i[0] for i in preds_labels]
+    labels = [i[1] for i in preds_labels]
+    L_POSSIBLE_BEHAVIORS.sort()
+    plot_confusion_matrix(preds, labels, L_POSSIBLE_BEHAVIORS,
+                            title='Confusion matrix - Inception3D',
+                            xlabel='I3D predictions',
+                            ylabel='Ground truth',
+                            out_file='cnf_matrix.png'
+                            )
 
+def main():
+    pickle_path = sys.argv[1]
+    confusion_matrix(pickle_path)
--- tools/utils.py
+++ tools/utils.py
@@ -3,7 +3,7 @@ import glob
 import h5py
 import numpy as np
 import tensorflow as tf
-
+import itertools
 #Generic utils file for I/O, pickle read, and other numeric computations
 
 def get_b2v(subset,DATASET_NAME):
@@ -48,3 +48,46 @@ def get_lists(subset,ratio):
     subset_labels = pickle.load(open(labels))[ind_s:ind_e]
     subset_videos = pickle.load(open(videos))[ind_s:ind_e]
     return subset_videos, subset_labels
+
+def plot_confusion_matrix(preds, labels, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          xlabel='Predictions',
+                          ylabel='Ground truth',
+                          cmap=plt.cm.Blues,
+                          out_file='cnf_matrix.png'):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    from sklearn.metrics import confusion_matrix
+    import matplotlib; matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+    
+    cm = confusion_matrix(preds,labels)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(out_file)
